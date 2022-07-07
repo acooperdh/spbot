f19d9d472ef0a17a8b889de38b41af2d31f992f3
Drew <an538926@dal.ca>
2022-07-07 09:38:56 -0300

    commiting changes

--- botv2/api/users/users.py
+++ botv2/api/users/users.py
@@ -3,6 +3,7 @@ import os
 import json
 import asyncio
 import disnake
+import time
 
 
 async def check_user_role(user, role):
@@ -11,7 +12,7 @@ async def check_user_role(user, role):
                            ).get_database("SimplePicks").Users
     find_user = users.find_one(
         {"discord_id": str(user["name"]) + "#" + str(user["discriminator"])})
-
+    print(find_user)
     if find_user is None or find_user["discord_role"] != role:
         return None
     return find_user
@@ -23,3 +24,11 @@ async def check_user_role(user, role):
     # asyncio.ensure_future(check_user_role(user, role))
     # loop.run_forever()
     # loop.close()
+
+
+# user = {"name": "Abdulrahman", "discriminator": "2474"}
+# role = "948740878736953404"
+# loop = asyncio.get_event_loop()
+# asyncio.ensure_future(check_user_role(user, role))
+# loop.run_forever()
+# loop.close()
